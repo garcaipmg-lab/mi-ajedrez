3ef6082555723cb85934cf05657828db5791e783
elitechesssoporte-dotcom <elitechess.soporte@gmail.com>
2026-07-05 12:17:40 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1276,9 +1276,9 @@ if __name__ == '__main__':
     print("="*50)
     
     # Para desarrollo local
-import os
-port = int(os.environ.get('PORT', 5000))
-socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
 
     
 
