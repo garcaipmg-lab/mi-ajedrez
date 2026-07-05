82cc8a25c7c213522de96130ea6b9340dccba2fe
elitechesssoporte-dotcom <elitechess.soporte@gmail.com>
2026-07-05 12:36:28 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1255,8 +1255,7 @@ def obtener_clasificacion(data):
         print(f"❌ Error al obtener clasificación: {e}")
         emit('clasificacion_response', {'categoria': categoria, 'jugadores': []})
 
-# Inicializar SocketIO para producción (Render)
-socketio.init_app(app, cors_allowed_origins="*")
+
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
