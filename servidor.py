4dc1244975639235bd793f15bd0339f0903bfb72
elitechesssoporte-dotcom <elitechess.soporte@gmail.com>
2026-07-10 16:40:09 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1498,6 +1498,7 @@ if __name__ == '__main__':
     print(f"📱 Otros PCs: http://{ip_local}:5000")
     print("="*50)
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 10000))
+socketio.run(app, host='0.0.0.0', port=port, debug=False)
     
     
