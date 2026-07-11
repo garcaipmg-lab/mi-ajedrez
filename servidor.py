2f933a576c42161a7127b9c7b83e012edb88a3b4
elitechesssoporte-dotcom <elitechess.soporte@gmail.com>
2026-07-11 13:40:28 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -8,7 +8,7 @@ import hashlib
 import os
 import time
 import threading
-
+from supabase import create_client, Client
 app = Flask(__name__, static_folder='.', static_url_path='')
 app.secret_key = 'elitechess_secreto_2026'
 socketio = SocketIO(app, cors_allowed_origins="*", ping_interval=5, ping_timeout=10)
